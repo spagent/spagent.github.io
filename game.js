1ad7f7a8f6e4a584eb486f08696cb6e3c1f83c6c
Richard <baez-z@hotmail.com>
2014-11-26 22:07:27 -0600

    Key up
    Cuarto intento

--- game.js
+++ game.js
@@ -474,8 +474,8 @@ function Ship() {
 					this.x = this.canvasWidth - this.width;
 			} else if (KEY_STATUS.up) {
 				this.y -= this.speed
-				if (this.y <= this.canvasHeight - this.height)
-					this.y = this.canvasHeight - this.height;
+				if (this.y <= this.canvasHeight + this.height)
+					this.y = this.canvasHeight + this.height;
 			} else if (KEY_STATUS.down) {
 				this.y += this.speed
 				if (this.y >= this.canvasHeight - this.height)
